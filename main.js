cfce253662c6440a59f55811711488f2f5bc560c
Мария Шишова <maryshish17@gmail.com>
2025-01-21 18:44:42 +0300

    ok

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+// ДЗ 1
 // Задача 1
 let str = "abcde";
 
-console.log(str[0]); 
-console.log(str[1]); 
+console.log(str[0]);
+console.log(str[1]);
 console.log(str[4]);
 
 // Задача 2
@@ -10,16 +11,21 @@ let num1 = parseFloat(prompt("Первое число:"));
 let num2 = parseFloat(prompt("Второе число:"));
 
 if (num1 > 0 || num2 > 0) {
-  console.log(true); 
+  console.log(true);
 } else {
-  console.log(false); 
+  console.log(false);
 }
 
 // Задача 3
 let age = parseInt(prompt("Твой возраст:"));
 
 if (age >= 18) {
-  console.log("Совершеннолетний"); 
+  console.log("Совершеннолетний");
 } else {
-  console.log("Несовершеннолетний"); 
+  console.log("Несовершеннолетний");
 }
+
+// ДЗ 2
+// ДЗ 3
+// ДЗ 4
+// ДЗ 5
